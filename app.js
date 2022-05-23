002221e7248d4e08496c310e2820fdf2d2f3c635
nyf! <67359464+nyf-tech@users.noreply.github.com>
2022-05-23 21:51:23 +0200

    add panel button

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/view/index.html`);
 });
 app.listen(80, () => {
-    console.log('Application listening on port 3333!');
+    console.log('Application listening on port 80!');
 });
