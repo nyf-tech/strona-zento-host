cd8088672033e3876576ca42a6563ff610d23681
nyf! <67359464+nyf-tech@users.noreply.github.com>
2022-05-23 14:11:02 +0200

    XD

---
+++ app.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const port = 80;
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/view/index.html`);
+});
+app.listen(80, () => {
+    console.log('Application listening on port 3333!');
+});
